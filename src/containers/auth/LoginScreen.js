3a6ea4a40ccc79ee92ffdc6d8f4a53de47720e29
Willane Paiva <willanepaiva@hotmail.com>
2018-02-02 22:57:46 -0300

    Add New Components(User, Pass input, labels)

--- src/containers/auth/LoginScreen.js
+++ src/containers/auth/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
 
 class LoginScreen extends Component {
   constructor(props) {
@@ -9,11 +9,30 @@ class LoginScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Welcome to the jungle.</Text>
+
+        <Text style={styles.text}> Nome do usuário </Text>
+
+        <TextInput 
+          style={styles.usuario}
+          placeholder="Insira aqui o nome do usuário"
+        />
+
+        <Text 
+        style={styles.text}>
+        Senha
+        </Text>
+
+        <TextInput 
+          style={styles.usuario}
+          secureTextEntry = {true}
+          placeholder="Insira aqui a senha do usuário"
+        />
+
         <Button
           onPress={() => this.props.navigation.navigate('Dashboard')}
           title="Login"
         />
+
       </View>
     )
   }
@@ -25,7 +44,19 @@ const styles = {
     justifyContent: 'center',
     alignItems: 'center',
     height: '100%'
+  },
+
+  text:{
+    fontWeight : 'bold',
+    color: '#0084b4',
+  },
+
+  usuario:{
+    height: '8%',
+    width: '80%',
+    
   }
+
 }
 
 export default LoginScreen
